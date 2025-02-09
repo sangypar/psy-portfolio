3f9eb150f82ac779bcc7b439d86a80aae89a214b
박상용 <psyok7@naver.com>
2025-02-10 02:45:36 +0900

    gitpage 배포 문제 해결

--- psy-portfolio/next.config.js
+++ psy-portfolio/next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+const repository = 'psy-portfolio'; // 여기에 저장소 이름을 입력하세요
+
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
+    loader: 'custom',
+    loaderFile: './image-loader.js',
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/psy-portfolio' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/psy-portfolio' : '',
+  assetPrefix: isProduction ? `/${repository}/` : '',
+  basePath: isProduction ? `/${repository}` : '',
   trailingSlash: true,
 }
 
