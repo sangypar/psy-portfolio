44134fe01563d65758685326017228731c2b8758
박상용 <psyok7@naver.com>
2025-02-10 03:49:08 +0900

    gitpage 배포 문제 해결

--- psy-portfolio/next.config.js
+++ psy-portfolio/next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/psy-portfolio' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/psy-portfolio' : '',
   trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  // PostCSS와 Tailwind 설정 추가
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
 }
 
 module.exports = nextConfig
