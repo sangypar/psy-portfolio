fc821c6699a479987bb53f85ed7b39c0a68f710e
박상용 <psyok7@naver.com>
2025-02-10 17:32:49 +0900

    fix deploy

--- next.config.js
+++ next.config.js
@@ -2,16 +2,13 @@ const repo = 'psy-portfolio'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export',  
   images: {
     unoptimized: true,
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
   basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
   trailingSlash: true,
-  distDir: 'dist',
-  reactStrictMode: true,
-  swcMinify: true,
 }
 
 module.exports = nextConfig
